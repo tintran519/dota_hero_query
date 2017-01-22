e4bf6c8e6bcc4a42257d4e313a78fc9a9412a148
Tin Tran <tin.tran0519@gmail.com>
2017-01-22 00:11:52 -0800

    reformat route complete

--- react_app/server/routes.js
+++ react_app/server/routes.js
@@ -1,4 +1,9 @@
 var express = require('express');
     router  = new express.Router();
 
+//require controllers
+var heroController  = require('./controllers/heroes');
 
+router.get('/heroes', heroController.index);
+
+module.exports = router
